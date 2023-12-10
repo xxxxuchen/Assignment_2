4f439ec5c03702064d1627c80b09c49631ebfdc0
Xu Chen <barrychen0107@gmail.com>
2023-12-10 00:39:09 -0500

    remove useless comments

--- sut.c
+++ sut.c
@@ -1,7 +1,7 @@
 /**
+ * User-level thread library
  * Author: Xu Chen
- * McGill ID: 260952566
- * TODO: implement more robust error handling
+ * Date: 2023/11/05
  */
 #include <pthread.h>
 #include <signal.h>
@@ -288,62 +288,3 @@ void sut_shutdown() {
   free(I_context);
   free(I_stack);
 }
-
-/*--------------------Test Below---------------------*/
-
-/*
-void hello3() {
-  int i, fd;
-  int buf_size = 2048;
-  char read_sbuf[buf_size];
-  fd = sut_open("./test5.txt");
-  sut_yield();
-  if (fd < 0)
-    printf("Error: sut_open() failed in hello3()");
-  else {
-    char *read_result = sut_read(fd, read_sbuf, buf_size);
-    if (read_result != NULL) {
-      printf("%s\n", read_sbuf);
-      sut_close(fd);
-    } else {
-      printf("Error: sut_read() failed");
-    }
-  }
-  sut_exit();
-}
-
-void hello1() {
-  int i, fd;
-  char write_sbuf[128];
-  fd = sut_open("./test5.txt");
-  if (fd < 0)
-    printf("Error: sut_open() failed \n");
-  else {
-    for (i = 0; i < 5; i++) {
-      sprintf(write_sbuf, "Hello world!, message from SUT-One i = %d \n", i);
-      sut_write(fd, write_sbuf, strlen(write_sbuf));
-      sut_yield();
-    }
-    sut_close(fd);
-    sut_create(hello3);
-  }
-
-  sut_exit();
-}
-
-void hello2() {
-  int i;
-  for (i = 0; i < 5; i++) {
-    printf("Hello world!, this is SUT-Two \n");
-    sut_yield();
-  }
-  sut_exit();
-}
-
-int main() {
-  sut_init();
-  sut_create(hello1);
-  sut_create(hello2);
-  sut_shutdown();
-}
-*/
